8f3b96129f07896323199a40fffe208ff59df164
HarVard <har19vard90@gmail.com>
2023-03-27 11:39:01 +0300

    add:create homepage file

--- front-app/src/router/index.js
+++ front-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HomePage from '../views/HomePage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/homepage',
+    name: 'homepage',
+    component: HomePage,
   },
   {
     path: '/about',
---
+++ front-app/src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <!-- Your homepage content goes here -->
+    </div>
+  </template>
+  
+<script>
+  export default {
+    name: 'HomePage',
+    components: {
+      // Import and use any components you need for your homepage
+    },
+  };
+</script>
+  
+<style>
+  /* Your homepage CSS styles go here */
+</style>
+  
